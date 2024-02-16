2cdb9c057eefb00ea5764d21088879276d31967f
Serhieie <89160714+Serhieie@users.noreply.github.com>
2024-02-16 22:38:39 +0200

    priceAverage

--- models/asset.js
+++ models/asset.js
@@ -19,7 +19,7 @@ const assetSchema = new Schema(
       type: Number,
       required: true,
     },
-    priceAvarenge: {
+    priceAverage: {
       type: Number,
       required: true,
     },
@@ -61,7 +61,41 @@ const assetSchema = new Schema(
 
 const Joi = require("joi");
 
+// type Asset = {
+//   amount: number,
+//   assetId: string,
+//   date: number,
+//   grow: boolean,
+//   growPercent: number,
+//   icon: string,
+//   name: string,
+//   owner: {
+//     _id: string,
+//     name: string,
+//     email: string,
+//   },
+//   price: number,
+//   priceAverage: number, // Змінив назву поля, оскільки "priceAvarenge" містить орфографічну помилку
+//   symbol: string,
+//   totalAmount: number,
+//   totalProfit: number,
+//   _id: string,
+// };
+
+// amount: 1;
+// assetId: "wrapped-bitcoin";
+// date: 1708100738376;
+// grow: true;
+// growPercent: 0;
+// icon: "https://static.coinstats.app/coins/wrapped-bitcoinoc1.png";
+// name: "Wrapped Bitcoin";
+// price: 52178;
+// priceAvg: 52178;
+// symbol: "WBTC";
+// totalAmount: 52178;
+// totalProfit: 0;
 const createAssetSchema = Joi.object({
+  _id: Joi.string(),
   assetId: Joi.string().required(),
   amount: Joi.number().required(),
   price: Joi.number().required(),
@@ -71,12 +105,13 @@ const createAssetSchema = Joi.object({
   totalProfit: Joi.number().required(),
   grow: Joi.boolean().required(),
   name: Joi.string().required(),
-  priceAvarenge: Joi.number().required(),
+  priceAverage: Joi.number().required(),
   icon: Joi.string().required(),
   symbol: Joi.string().required(),
 });
 
 const updateAssetSchema = Joi.object({
+  _id: Joi.string(),
   assetId: Joi.string(),
   amount: Joi.number(),
   price: Joi.number(),
@@ -84,9 +119,10 @@ const updateAssetSchema = Joi.object({
   growPercent: Joi.number(),
   totalAmount: Joi.number(),
   totalProfit: Joi.number(),
+  owner: Joi.array(),
   grow: Joi.boolean(),
   name: Joi.string(),
-  priceAvarenge: Joi.number(),
+  priceAverage: Joi.number(),
   icon: Joi.string(),
   symbol: Joi.string(),
 });

d8e6ba1217839021d7439df37f62346759a43b30
Serhieie <89160714+Serhieie@users.noreply.github.com>
2024-02-20 23:33:09 +0200

    fix auth

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -167,7 +167,7 @@ const passwordChanging = async (req, res) => {
 
 const changeSubscription = async (req, res) => {
   const { _id } = req.user;
-  const result = await User.findByIdAndUpdate(_id, req.body);
+  const result = await User.findByIdAndUpdate(_id, req.body, { new: true });
   if (!result) {
     throw HttpError(404);
   }

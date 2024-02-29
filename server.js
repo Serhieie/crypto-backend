b48c89f1d256b45913955350b350a046c911b12f
Serhieie <89160714+Serhieie@users.noreply.github.com>
2024-02-29 13:18:42 +0200

    fiax

--- server.js
+++ server.js
@@ -1,36 +1,13 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 const { DB_HOST, PORT } = process.env;
-const http = require("http");
-const { Server } = require("socket.io");
 
 mongoose.set("strictQuery", true);
-
-const httpServer = http.createServer(app);
-const listenThis = app.get("env") === "development" ? 3021 : process.env.PORT;
-
-const io = new Server(httpServer, {
-  cors: {
-    origin: "*",
-  },
-});
-
-io.listen(PORT, () => {
-  console.log(`server running at ${PORT}`);
-});
-
-io.on("connection", (socket) => {
-  console.log("A user connected");
-
-  socket.on("chat-message", (message) => {
-    io.emit("chat-message", message);
-  });
-});
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    httpServer.listen(PORT);
+    app.listen(PORT);
   })
   .catch((error) => {
     console.log(error.message);
